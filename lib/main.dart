108dbb17e76ff5cd6397b6088e257e9169f04e9b
Luis.souza03 <luis93667@gmail.com>
2022-08-21 16:02:33 -0300

    feat: add setState

--- lib/main.dart
+++ lib/main.dart
@@ -46,58 +46,63 @@ class _BudgetScreenState extends State<BudgetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Budget Tracker'),
-      ),
-      body: FutureBuilder<List<Item>>(
-        future: _futureItems,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            // Show pie chart and list view of items.
-            final items = snapshot.data!;
-            return ListView.builder(
-              itemCount: items.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) return SpendingChart(items: items);
+        appBar: AppBar(
+          title: const Text('Budget Tracker'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: () async {
+            _futureItems = BudgetRepository().getItems();
+            setState(() {});
+          },
+          child: FutureBuilder<List<Item>>(
+            future: _futureItems,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // Show pie chart and list view of items.
+                final items = snapshot.data!;
+                return ListView.builder(
+                  itemCount: items.length + 1,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) return SpendingChart(items: items);
 
-                final item = items[index - 1];
-                return Container(
-                  margin: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                        width: 2.0, color: getCategoryColor(item.category)),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
-                        blurRadius: 6.0,
-                      )
-                    ],
-                  ),
-                  child: ListTile(
-                    title: Text(item.name),
-                    subtitle: Text(
-                      '${item.category} - ${DateFormat('dd/MM/yyyy').format(item.date)} ',
-                    ),
-                    trailing: Text(
-                      '-\$${item.price.toStringAsFixed(2)}',
-                    ),
-                  ),
+                    final item = items[index - 1];
+                    return Container(
+                      margin: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            width: 2.0, color: getCategoryColor(item.category)),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          )
+                        ],
+                      ),
+                      child: ListTile(
+                        title: Text(item.name),
+                        subtitle: Text(
+                          '${item.category} - ${DateFormat('dd/MM/yyyy').format(item.date)} ',
+                        ),
+                        trailing: Text(
+                          '-\$${item.price.toStringAsFixed(2)}',
+                        ),
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-          } else if (snapshot.hasError) {
-            // Show failure error message.
-            final failure = snapshot.error as Failure;
-            return Center(child: Text(failure.message));
-          }
-          // Show a loading spinner
-          return const Center(child: CircularProgressIndicator());
-        },
-      ),
-    );
+              } else if (snapshot.hasError) {
+                // Show failure error message.
+                final failure = snapshot.error as Failure;
+                return Center(child: Text(failure.message));
+              }
+              // Show a loading spinner
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        ));
   }
 }
 
@@ -115,3 +120,27 @@ Color getCategoryColor(String category) {
       return Colors.orange[400]!;
   }
 }
+
+// Color getCategoryColor(String category) {
+//   switch (category) {
+//     case 'Supérfluo':
+//       return Colors.red[400]!;
+//     case 'Comida e bebida':
+//       return Colors.green[400]!;
+//     case 'Educação':
+//       return Colors.blue[400]!;
+//     case 'Transporte':
+//       return Colors.purple[400]!;
+//     case 'Mercado':
+//       return Colors.yellow[400]!;
+//     case 'Shopping':
+//       return Colors.pink[400]!;
+//     case 'Saúde':
+//       return Colors.blueAccent[400]!;
+//     case 'Pagamento Fatura':
+//       return Colors.lightGreenAccent[400]!;
+      
+//     default:
+//       return Colors.orange[400]!;
+//   }
+// }
